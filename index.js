ba047583514bf685694cc1a7a267cf3eb809547f
Nazareno Valentini <nazareno.valentini@gmail.com>
2023-04-06 12:32:17 +0200

    3.5

--- index.js
+++ index.js
@@ -41,18 +41,35 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
-}
-)
+})
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(p => p.id === id)
     persons = persons.filter(p => p !== person)
     response.status(204).end()
+})
 
+const generateId = () => {
+    return Math.floor(Math.random() * 10000000)
 }
 
-)
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person)
+    response.json(person)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
